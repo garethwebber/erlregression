c98a5235d3a5b6fdb70ba2ee7e91791648125f3f
Gareth Webber <github@garethwebber.com>
2018-06-11 19:27:46 +0100

    Typo in debug string.

--- fac.erl
+++ fac.erl
@@ -15,7 +15,7 @@ printPoint({point,PX,PY}) ->
 regression(L) ->
   N = length(L),
   {regressionterms, X, Y, XY, X2, Y2} = terms(L),
-  io:format("X=~w Y=~w XY=~w X2=~w Y2=~w N=~w ~n", [X, Y, XY, X2, Y, N]),
+  io:format("X=~w Y=~w XY=~w X2=~w Y2=~w N=~w ~n", [X, Y, XY, X2, Y2, N]),
   A = (Y*X2-X*XY) / (N*X2-X2),
   B = (N*XY - X*Y) / (N*X2-X2),
   {regression, A, B}.
