56680fe15b7d9e20ffe5598a57dfe3f20846c80f
Gareth Webber <github@garethwebber.com>
2018-07-20 18:06:23 +0200

    Fix regression formula S(X^2) to (SX)^2

--- src/regression_math.erl
+++ src/regression_math.erl
@@ -6,8 +6,9 @@ regression(L) ->
   N = length(L),
   {regressionterms, X, Y, XY, X2, Y2} = terms(L),
   % io:format("X=~w Y=~w XY=~w X2=~w Y2=~w N=~w ~n", [X, Y, XY, X2, Y2, N]),
-  A = (Y*X2-X*XY) / (N*X2-X2),
-  B = (N*XY - X*Y) / (N*X2-X2),
+  B = ((N*XY) - (X*Y))  / ((N*X2)-X*X),
+  %A = ((Y*X2) - (X*XY)) / ((N*X2)-X2),
+  A = (Y / N) - ((B * X)/N),
   {regression, A, B}.
 
 % Create sums of X, Y, XY, X square and Y squared from a list of points 
