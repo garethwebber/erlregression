9b558d5157c714b2c4b3292a88b2c39eecb8c576
Gareth Webber <github@garethwebber.com>
2018-07-20 13:38:37 +0200

    More Material UI: moved from CSS grid to Material Grid.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Button, TextField, Typography} from '@material-ui/core';
+import {AppBar, Button, Grid, List, ListItem, TextField, Typography} from '@material-ui/core';
+import 'typeface-roboto';
 import './App.css';
 
 export default class App extends React.Component {
@@ -32,29 +33,34 @@ export default class App extends React.Component {
     const graph = this.state.graph;
     return (
       <div className="App">
-        <header className="App-header">
-	   <Typography variant='display1' align='center' gutterBottom>
+        <AppBar position="static"> 
+	   <Typography variant='display1' align='center' color="inherit" gutterBottom>
               Erlang Regression App            
            </Typography>
-        </header>
-        <div className="leftcolumn">
+        </AppBar>
+	<Grid container xs={12} spacing={24444}>
+	<Grid item xs={6}>
           <Typography variant="title" gutterBottom>
 	  There are {count} points:
 	  </Typography>
-          
+         
+	  <List>
           {points.map((point, index) => (
-            <p key={index}>
+            <ListItem key={index}>
 	    (
               {point.point.x}
             ,
               {point.point.y}
-            )&nbsp;
+            )
             <img width="16" height="16" alt="delete"
 		  id={'[{"point" : { "x" : ' + point.point.x + ', "y" : ' + point.point.y + '}}]'}
 		  src="/static/trash-can.png"
 		  onClick={this.handleDelete}/>
-            </p>
+
+            </ListItem>
           ))}
+          </List>
+
           <p>
             {regression}
           </p>
@@ -62,9 +68,9 @@ export default class App extends React.Component {
   <TextField name="x" label="X" value={this.state.x} onChange={this.handleChange} margin="normal"/>
   <TextField name="y" label="Y" yalue={this.state.y} onChange={this.handleChange} margin="normal"/>
   <Button variant="raised" color="primary" type="submit">Add Point</Button>
-</form> 
-	    </div>
-        <div className="rightcolumn">
+</form>
+          </Grid>
+	  <Grid item xs={6}>
           {count < 3
 	&& (
 <span>
@@ -77,8 +83,8 @@ export default class App extends React.Component {
 	)
 	}
           {count > 2 && <img src={graph} />}
-        </div>
-        <div className="footer">
+	</Grid>
+        <Grid item xs={12}>
           <hr />
 	  <Typography align='center' gutterBottom>
           Erlang/React example application by Gareth Webber. 
@@ -88,7 +94,8 @@ export default class App extends React.Component {
 	    Swagger <a href="/api-docs">API documentation</a> for the
 	    Elang provide REST interface.
           </Typography> 
-        </div>
+	  </Grid>
+	  </Grid>
       </div>
     );
   }
